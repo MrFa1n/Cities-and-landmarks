3872494b2f5011c626ed627d58d5d617929ff386
lpjak <lpjakewolfskin@gmail.com>
2021-06-10 01:12:19 +0300

    remove CEO

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/register', 'Api\AuthController@register');
 Route::post('/login', 'Api\AuthController@login');
 
-Route::apiResource('/ceo', 'Api\CEOController')->middleware('auth:api');
+//Route::apiResource('/ceo', 'Api\CEOController')->middleware('auth:api');
