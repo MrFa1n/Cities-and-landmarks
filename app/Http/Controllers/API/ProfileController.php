d64e45fe24671e4293dc3484a81329caeb0bf892
MrFa1n <manasasin1@gmail.com>
2021-08-13 21:32:49 +0300

    edit recommendations

--- app/Http/Controllers/API/ProfileController.php
+++ app/Http/Controllers/API/ProfileController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Validator;
 use App\Http\Resources\ProfileResource;
 use App\Models\HashTagsProfilModel;
 use App\Models\UploadPhotoModel;
+use Illuminate\Support\Facades\DB;
 
 class ProfileController extends Controller
 {
@@ -87,21 +88,71 @@ class ProfileController extends Controller
 
         $profile_id = $data['profile_id'];
         $age_pref = explode(':', ProfileFields::where('profile_id', $profile_id)->where('field_type_id', '5')->get()->first()->value);
-        // return response(['status' => 'ok', 'response' => ['profile_id' => $profile_id, 'profiles' => $age_pref]]);
+        //return response(['status' => 'ok', 'response' => ['profile_id' => $profile_id, 'profiles' => $age_pref]]);
         
         $sex_pref = explode(',', ProfileFields::where('profile_id', $profile_id)->where('field_type_id', '6')->get()->first()->value);
         //return response(['status' => 'ok', 'response' => ['profile_id' => $profile_id, 'profiles' => $sex_pref]]);
+        //return $sex_pref;
         //orWhere('value', $sex_pref)->
-
-        $hashtag = 1;
-
+/*
         $recs_age = ProfileFields::
             where('field_type_id', '1')->
             whereBetween('value', $age_pref)->
-            where('profile_id', '<>', $profile_id)->
-            get();
+            where('profile_id', '<>', $profile_id);
 
-        return response(['status' => 'ok', 'response' => ['profile_id' => $profile_id, 'profiles' => $recs_age]]);
+        $recs_sex = ProfileFields::
+            where('field_type_id', '3')->
+            where('value', $sex_pref)->
+            where('profile_id', '<>', $profile_id)->
+            union($recs_age)->
+            groupBy('profile_id')->
+            get();
+        */
+        /*
+        $recs_age = ProfileFields::
+            where(function ($q) use ($age_pref) {
+                $q->where('field_type_id', '1')->
+                    whereBetween('value', $age_pref);
+            })->
+            orWhere(function ($q) use ($sex_pref) {
+                $q->where('field_type_id', '3')->
+                    where('value', $sex_pref);
+            })
+            ->where('profile_id', '<>', $profile_id)->get();
+
+        */
+        /*
+        $recs_age = ProfileFields::
+    select('profile_id')->
+    where(function ($q) use ($age_pref, $profile_id) {
+        $q->where('field_type_id', '1')->
+            whereBetween('value', $age_pref)->
+            where('profile_id', '<>', $profile_id);
+    })->
+    where(function ($q) use ($sex_pref, $profile_id) {
+        $q->where('field_type_id', '3')->
+            where('value', $sex_pref)->
+            where('profile_id', '<>', $profile_id);
+    })
+    ->distinct()
+    ->get(['profile_id']);*/
+    //toSql()
+        //$all_recs = $recs_age->merge($recs_sex);
+        $sex_pref1 = $sex_pref[0];
+        //return $sex_pref1;
+        $age_pref[0] = $age_pref[0];
+        $age_pref[1] = $age_pref[1];
+        $results = DB::select( DB::raw("SELECT distinct p.* FROM 
+            profiles p 
+            JOIN profile_fields pf_age ON pf_age.`profile_id` = p.`id` and pf_age.`field_type_id` = 1 and pf_age.`value` BETWEEN :age_from AND :age_to
+            JOIN profile_fields pf_sex ON pf_sex.`profile_id` = p.`id` and pf_sex.`field_type_id` = 3 and pf_sex.`value` = :sex
+            WHERE p.`id` != $profile_id and  (pf_age.`id` IS NOT NULL and pf_sex.`id` IS NOT NULL)"), array(
+            'age_from' => $age_pref[0],
+            'age_to' => $age_pref[1],
+            'sex' => $sex_pref1,
+        ));
+
+        return response(['status' => 'ok', 'response' => ['profile_id' => $profile_id, 'profiles' => $results]]);
     }
 
     /**
