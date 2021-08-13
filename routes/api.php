f92eae8ba28532edbfc0a47bcdfd97a5feb9908d
MrFa1n <manasasin1@gmail.com>
2021-08-13 21:35:46 +0300

    update_routes

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| is assigned the "API" middleware group. Enjoy building your API!
 |
 */
 
@@ -19,22 +19,22 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 //POST
-Route::post('/register', 'Api\AuthController@register');
-Route::post('/register_profile', 'Api\ProfileController@register_profile');
-Route::post('/login', 'Api\AuthController@login');
-Route::post('/match', 'Api\MatchController@match_found');
-Route::post('/photo_up', 'Api\ProfileController@upload_photo');
-Route::post('/tag_add', 'Api\ProfileController@add_hashtag');
-Route::post('/gift', 'Api\GiftController@give_a_gift');
-Route::post('/edit_profile', 'Api\ProfileController@update');
+Route::post('/register', 'API\AuthController@register');
+Route::post('/register_profile', 'API\ProfileController@register_profile');
+Route::post('/login', 'API\AuthController@login');
+Route::post('/match', 'API\MatchController@match_found');
+Route::post('/photo_up', 'API\ProfileController@upload_photo');
+Route::post('/tag_add', 'API\ProfileController@add_hashtag');
+Route::post('/gift', 'API\GiftController@give_a_gift');
+Route::post('/edit_profile', 'API\ProfileController@update');
 
 //GET(GAY)
-Route::get('/get_match', 'Api\MatchController@get_match');
-Route::get('/get_gift', 'Api\GiftController@get_gift');
-Route::get('/get_profile', 'Api\ProfileController@get_profile');
-Route::get('/get_rec', 'Api\ProfileController@get_recomendations');
-Route::get('/profile_gifts', 'Api\GiftController@profile_gifts');
-Route::get('/get_tier_gifts', 'Api\GiftController@get_tier');
-Route::get('/lute_box', 'Api\GiftController@lutebox');
-Route::get('/get_all_photo_profile', 'Api\ProfileController@get_photo');
+Route::get('/get_match', 'API\MatchController@get_match');
+Route::get('/get_gift', 'API\GiftController@get_gift');
+Route::get('/get_profile', 'API\ProfileController@get_profile');
+Route::get('/get_rec', 'API\ProfileController@get_recomendations');
+Route::get('/profile_gifts', 'API\GiftController@profile_gifts');
+Route::get('/get_tier_gifts', 'API\GiftController@get_tier');
+Route::get('/lute_box', 'API\GiftController@lutebox');
+Route::get('/get_all_photo_profile', 'API\ProfileController@get_photo');
 
